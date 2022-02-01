895b9894209a9d7a15bed05fbe394352f90bf117
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-01 10:57:15 -0800

    Completed javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,35 @@
+var orderHistory = [{
+  itemName: 'JavaScript for impatient programmers',
+  author: 'Rauschmayer, Dr. Axel',
+  window: 'Return window closed on sep 7, 2020',
+  date: 'Aug 8, 2020',
+  price: '$31.55',
+  total: '$34.00'
+
+},
+{
+  itemName: 'The timeless Way of Building',
+  author: 'Alexander, Christopher',
+  widnow: 'Return window closed on Aug 19, 2020',
+  date: 'July 20, 2020',
+  price: '$41.33',
+  total: '$44.53'
+
+},
+{
+  itemName: 'gamecube controller adapter',
+  window: 'Closed on Aug 5, 2020',
+  date: 'July 7, 2020',
+  price: '$15.98',
+  total: '17.22'
+
+},
+{
+  itemName: '"Gamecube cotroller", "The Art of Sql"',
+  window: 'Return window closed on aug 4, 2020',
+  date: 'July 5 2020',
+  price: ' "$94.95", "$33.99" ',
+  total: '$138.93'
+}
+
+];
