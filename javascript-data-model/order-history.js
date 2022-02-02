70ef22f6a8ab1ebd534821cd0f9501a1b7b1fc02
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-01 16:45:13 -0800

    In  progress

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,2 +1,39 @@
 var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    total: 34.00,
+    recepient: 'JS Masher',
+    orderNumber: '114-3941689-87772232',
+    item: [
+      {
+        type: 'Book',
+        title: 'JavaScript for Impatient programmers',
+        author: 'Raushmayer, Dr. Axel',
+        returnUntil: 'Sep, 7 2020',
+        priceofItem: 31.55
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 19, 2020',
+    total: 44.53,
+    recipient: 'JS Masher',
+    orderNumber: '113-9984268-128057',
+    item: [
+      {
+        type: 'Book',
+        title: 'The Tmeless Way of Building',
+        author: 'Alexander, Chritopher',
+        returnUntil: 'Aug 19, 2020',
+        priceofItem: 41.53
+      },
+      {
+        orderPlaced: 'July 4, 2020',
+        total: 17.22,
+        recipient: 'JS Masher'
+
+      }
+    ]
+  }
+
 ];
