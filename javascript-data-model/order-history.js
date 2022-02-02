e7cfe724ac2f65fe1fad6f650473ec1d5d672620
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-02 00:22:02 -0800

    Completed javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -26,12 +26,40 @@ var orderHistory = [
         author: 'Alexander, Chritopher',
         returnUntil: 'Aug 19, 2020',
         priceofItem: 41.53
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    total: 17.22,
+    recipient: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    item: [
+      {
+        type: 'hardware',
+        name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC, SUpport Turbo and Vibration Features. No Driving and No-lag-Gamecube Adapter',
+        returnUntil: 'Aug 5, 2020',
+        priceOfItem: 15.98
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    total: 138.93,
+    recipient: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    items: [
+      {
+        type: 'hardware',
+        returnUntil: 'Aug 4, 2020',
+        priceOfitem: 94.95,
+        name: 'GameCube Controller - Super Smash Bros.Edition(Nintendo Switch'
       },
       {
-        orderPlaced: 'July 4, 2020',
-        total: 17.22,
-        recipient: 'JS Masher'
-
+        type: 'book',
+        returnUntil: 'Aug 4, 2020',
+        name: 'The Art of Sql',
+        priceOfItem: 33.99
       }
     ]
   }
