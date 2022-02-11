00b159b81cbce1d6e8ac24109b1efff4bc240132
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-10 18:00:34 -0800

    In progres

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,12 @@
+/*  var tab = document.querySelector('.tab');
+var view = document.querySelector('.view');
+console.log(view);
+console.log(tab);
+
+tabcontainer.addEventListener('click', function (e) {
+if(tabcontainer.matches('tab')) {
+  return
+}
+var tabcontainer = document.querySelector('.tab-container');
+}
+*/
