8ac703574531735b7bed3a885bbd6512dfcbc075
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-11 12:34:03 -0800

    In progress

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,11 +1,15 @@
 var div = document.querySelector('.firstdiv');
+var body = document.querySelector('#back');
 var isBulbOn = false;
 function handleClick(event) {
   /* console.log('event was fired'); */
   if (isBulbOn === true) {
     div.className = 'firstdiv';
+
   } else if (isBulbOn === false) {
     div.className = 'firstdiv off ';
+    body.className = '.back';
+
   }
   isBulbOn = !isBulbOn;
 }
