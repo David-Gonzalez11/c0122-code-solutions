96738a4367e22ae9debd75cf14d64f7544c04017
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-11 13:33:27 -0800

    in progress

---
+++ javascript-and-json/main.js
@@ -0,0 +1,34 @@
+// var books = [
+//   {
+//     title: 'This is my book',
+//     isbn: '1345-223-233',
+//     author: 'authors'
+//   },
+//   {
+//     title1: 'diary of a wimpy kid',
+//     isbn: '2333-444-555',
+//     author: 'jk wrowling'
+//   },
+//   {
+//     title: 'my title',
+//     isbn: '344-555-55',
+//     author: 'Me'
+//   }
+// ];
+// console.log('arr type:', typeof book);
+
+// console.log('JSON result:', typeof JSON.stringify({ books }));
+
+// [
+//   {
+//     "Number id": "this is the name"
+//   }
+// ]
+
+// "Number id: this is the name"
+// console.log(typeof "Number id": "this is the name")
+
+// console.log('student:', typeof student);
+
+// var parse = JSON.parse(student)
+// console.log('parse:', typeof parse)
