68956a17e2bf32fdc8ffff4974176051edc946f4
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-11 15:29:50 -0800

    In progress

--- javascript-and-json/main.js
+++ javascript-and-json/main.js
@@ -1,34 +1,29 @@
-// var books = [
-//   {
-//     title: 'This is my book',
-//     isbn: '1345-223-233',
-//     author: 'authors'
-//   },
-//   {
-//     title1: 'diary of a wimpy kid',
-//     isbn: '2333-444-555',
-//     author: 'jk wrowling'
-//   },
-//   {
-//     title: 'my title',
-//     isbn: '344-555-55',
-//     author: 'Me'
-//   }
-// ];
-// console.log('arr type:', typeof book);
+var books = [
+  {
+    title: 'This is my book',
+    isbn: '1345-223-233',
+    author: 'authors'
+  },
+  {
+    title1: 'diary of a wimpy kid',
+    isbn: '2333-444-555',
+    author: 'jk wrowling'
+  },
+  {
+    title: 'my title',
+    isbn: '344-555-55',
+    author: 'Me'
+  }
+];
+console.log('arr type:', typeof books);
+console.log('JSON result:', typeof JSON.stringify(books));
 
-// console.log('JSON result:', typeof JSON.stringify({ books }));
+var studentJSON = '{ "id": 1, "name": "Jeff"}';
 
-// [
-//   {
-//     "Number id": "this is the name"
-//   }
-// ]
+console.log('studentJSON:', studentJSON);
+console.log('typeof studentJSON:', typeof studentJSON);
 
-// "Number id: this is the name"
-// console.log(typeof "Number id": "this is the name")
+var student = JSON.parse(studentJSON);
 
-// console.log('student:', typeof student);
-
-// var parse = JSON.parse(student)
-// console.log('parse:', typeof parse)
+console.log('student:', student);
+console.log('typeof student:', typeof student);
