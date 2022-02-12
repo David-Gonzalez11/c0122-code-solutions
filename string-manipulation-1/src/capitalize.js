b635ddcdb2aa10c9228fbf7af86bb3244875e773
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-11 21:40:43 -0800

    Completed string-manipulation-1

--- string-manipulation-1/src/capitalize.js
+++ string-manipulation-1/src/capitalize.js
@@ -5,3 +5,14 @@
 // make the character you just grabbed and make it capital
 // split the words apart to target which chaeacter you need
 // retunr the result chracter(capitalized).
+function capitalize(word) {
+  word = word.split(' ');
+  let result = '';
+
+  for (let i = 0; i < word.length; i++) {
+    result +=
+      word[i].substring(0, 1).toUpperCase() +
+      word[i].substring(1).toLowerCase();
+  }
+  return result;
+}
