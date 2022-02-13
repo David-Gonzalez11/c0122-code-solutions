c24e633cf35a16ec795a04cb6617f544739d9c53
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-13 15:20:47 -0800

    In progress

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,26 @@
+// var typedletter = document.querySelectorAll('.letter');
+
+// var words = document.querySelector('.words');
+// var keypressed = false;
+
+// function keyDown(event) {
+//   for (var i = 0; i < typedletter.length; i++) {
+//     typedletter[i].addEventListener('keydown', keyDown);
+//   }
+//   if (keypressed === true) {
+//     typedletter.className = 'correct-char';
+//   } else if (keypressed === false) {
+//     typedletter.className = 'incorrect-char';
+//   }
+// }
+
+// if (typedletter == null) {
+//   typedletter.className.remove('correct-char');
+//   typedletter.className.remove('incorrect-char');
+// } else if (typedletter === typedletter.list.innerText) {
+//   typedletter.className.add('correct-char');
+//   typedletter.className.remove('incorrect-char');
+// } else {
+//   typedletter.className.remove('correc-chart');
+//   typedletter.className.add('incorrect-char');
+// }
