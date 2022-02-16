92d014ab3bbabc57b00884501208861a511835d1
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-16 06:35:28 -0800

    In progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,18 +1,17 @@
 // var typedletter = document.querySelectorAll('.letter');
 
 // var words = document.querySelector('.words');
-// var keypressed = false;
 
 // function keyDown(event) {
 
 // if (typedletter == null) {
 //   typedletter.className.remove('correct-char');
 //   typedletter.className.remove('incorrect-char');
-// } else if (typedletter === typedletter.list.innerText) {
+// } else if (typedletter === typedletter.textContent) {
 //   typedletter.className.add('correct-char');
 //   typedletter.className.remove('incorrect-char');
 // } else {
-//   typedletter.className.remove('correc-chart');
+//   typedletter.className.remove('correct-char');
 //   typedletter.className.add('incorrect-char');
 // }
-// typedletter.addEventListener()
+// typedletter.addEventListener('keydown', keyDown)
