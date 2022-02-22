b60f4c78c3219a49ab618f7dfbadf83a4d67a031
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-22 13:09:21 -0800

    In progress

--- javascript-constructors/example-constructor.js
+++ javascript-constructors/example-constructor.js
@@ -3,11 +3,17 @@ function ExampleConstructor() {
 }
 // var example = new ExampleConstructor();
 
+var newFunction = new ExampleConstructor();
+
 console.log(ExampleConstructor.prototype);
 console.log(typeof ExampleConstructor.prototype);
-var newFunction = new ExampleConstructor();
+
+console.log('typeof example.__proto__', typeof newFunction.__proto__);
+console.log('typeof example.__proto__', newFunction.__proto__);
+
 console.log('value of new function:', newFunction);
 var instance = (newFunction instanceof ExampleConstructor);
 console.log(instance);
 
 // type of example.__proto__//
+/// fix this//
