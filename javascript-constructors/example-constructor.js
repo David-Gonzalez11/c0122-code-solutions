0e4444ee1b5919c256e1e2fe0083755d9be6ef41
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-22 13:53:43 -0800

    In progress

--- javascript-constructors/example-constructor.js
+++ javascript-constructors/example-constructor.js
@@ -5,15 +5,12 @@ function ExampleConstructor() {
 
 var newFunction = new ExampleConstructor();
 
+console.log('typeof newFunction:', typeof newFunction);
+console.log('value of newFunction:', newFunction);
+
+console.log('typeof newFunction.__proto__', typeof newFunction.__proto__);
+console.log('typeof newFunction.__proto__', newFunction.__proto__);
 console.log(ExampleConstructor.prototype);
 console.log(typeof ExampleConstructor.prototype);
 
-console.log('typeof example.__proto__', typeof newFunction.__proto__);
-console.log('typeof example.__proto__', newFunction.__proto__);
-
-console.log('value of new function:', newFunction);
-var instance = (newFunction instanceof ExampleConstructor);
-console.log(instance);
-
-// type of example.__proto__//
-/// fix this//
+console.log('example instanceof:', newFunction instanceof ExampleConstructor);
