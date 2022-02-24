42194d29b9ae2b732e2a5b361eef5bf8aebf76de
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-24 09:36:41 -0800

    In progress

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,6 +1,34 @@
-function arrow() {
+// var array = ['images/025.png', 'images/039.png', 'images/007.png', 'images/004.png', 'images/001.png'];
+// var $img = document.querySelector('img');
+// var currentIndex = 0;
+// var $right = document.querySelector('.fa-angle-right');
+// $right.addEventListener('onclick:', arrowRight);
+// var $left = document.querySelector('.fa-angle-left');
+// $left.addEventListener('click', arrowLeft);
 
-}
-var left = document.querySelector('i');
-left.addEventListener('click', arrow);
-// console.log(left);
+// function arrowLeft(event) {
+//   if (currentIndex === 0) {
+//     currentIndex = array.length - 1;
+//     $img.setAttribute('src', array[4]);
+//   } else if (event.target === $left) {
+//     currentIndex--;
+//     $img.setAttribute('src', array[currentIndex]);
+//   }
+// }
+
+// function arrowRight(event) {
+//   if (currentIndex === array.length) {
+//     currentIndex = 0;
+//     $img.setAttribute('src', array[0]);
+//   } else if (event.target === $right) {
+//     currentIndex++;
+//     $img.setAttribute('src', array[currentIndex]);
+//   }
+// }
+
+// function circle(event) {
+
+// }
+// var circles = document.querySelector('.circles');
+// circles.addEventListener('click', circles);
+// console.log(circles);
