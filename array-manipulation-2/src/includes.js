3a2a0113b58da4281b730abd3f98b199a1022eef
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-25 22:49:21 -0800

    IN progress

---
+++ array-manipulation-2/src/includes.js
@@ -0,0 +1,5 @@
+// /* exported includes */
+// function includes(array, value) {
+//   const isInArray = array.includes(value);
+//   return isInArray;
+// }
---
+++ array-manipulation-2/src/take.js
@@ -0,0 +1,6 @@
+// /* exported take */
+// function take(array, count) {
+//   var arr = [];
+//   for(var i = 0; i < array.length; i++)
+
+// }
