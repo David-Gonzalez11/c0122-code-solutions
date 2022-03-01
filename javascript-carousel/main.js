74046928508c6bedf6ae9f6746669d434ededd60
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-28 21:59:30 -0800

    in progress

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -2,7 +2,7 @@ var array = ['images/025.png', 'images/039.png', 'images/007.png', 'images/004.p
 var $img = document.querySelector('img');
 var currentIndex = 0;
 var $right = document.querySelector('.fa-angle-right');
-$right.addEventListener('onclick:', arrowRight);
+$right.addEventListener('click', arrowRight);
 var $left = document.querySelector('.fa-angle-left');
 $left.addEventListener('click', arrowLeft);
 
@@ -10,34 +10,33 @@ function arrowLeft(event) {
   if (currentIndex === 0) {
     currentIndex = array.length - 1;
     $img.setAttribute('src', array[4]);
-  } else if (event.target === $left) {
+  } else{
     currentIndex--;
     $img.setAttribute('src', array[currentIndex]);
-    console.log('i was clicked');
   }
 }
 
 function arrowRight(event) {
-  if (currentIndex === array.length) {
-    currentIndex = 0;
-    $img.setAttribute('src', array[0]);
-  } else if (event.target === $right) {
-    currentIndex++;
-    $img.setAttribute('src', array[currentIndex]);
-    console.log('i was clicked');
-
+  if (currentIndex === array.length - 1 ) {
+currentIndex = 0;
   }
+   else {
+currentIndex++;
+  $img.setAttribute('src', array[currentIndex]);
+ }
 }
 
 function circle(event) {
-  console.log('I was clciked');
+// currentIndex = parseInt(event.target.id)
+// $img.setAttribute('src', array[currentIndex]);
+// console.log(event.target)
+if(event.target.tagName === ('BUTTON')){
+  currentIndex = parseInt(event.target.id)
+$img.setAttribute('src', array[currentIndex]);
 }
-var circles = document.querySelectorAll('.circles');
-circles.addEventListener('click', circle);
-console.log(circles);
 
-// target the arrrows to chnage through the images and indexes as each button is clicked
-// when the button on the right is clicked increement the current variables to be able tp display the next image at cettain index
-// when the button on th left is clicked decrement the current variable to be abkle to display the next image at the index before
-// add a query selector for all of the circles when they are clicked
-// when the right button matches the index at which it was clickjed then show that image
+}
+
+var circles = document.querySelector('.circles');
+circles.addEventListener('click', (e) =>  circle(e));
+console.log(circles)
