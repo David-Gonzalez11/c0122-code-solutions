2fde9977762f5189806f5cc9fb403418fbd932b5
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-03-01 19:52:33 -0800

    finished string-manipulation-2

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,8 +1,4 @@
 /* exported lastChars */
 function lastChars(length, string) {
-  var str = '';
-  for (var i = string.length - length; i < string.length; i++) {
-    str += string[i];
-  }
-  return str;
+  return string.slice(-length);
 }
